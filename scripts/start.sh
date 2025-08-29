dcab08faefc5e91b82e9c4c987f38f22c1f2a45b
psg-dsci <160556548+psg-dsci@users.noreply.github.com>
2025-08-29 15:25:15 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo apt install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
 cp -r /tmp/web/index.html /var/www/html/index.html
+sudo systemctl restart apache2
